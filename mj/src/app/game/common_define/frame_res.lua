042d12d85b644436e4f864d5ae47c96332b29922
ww <746277418@qq.com>
2016-11-27 23:37:10 +0800

    .

--- mj/src/app/game/common_define/frame_res.lua
+++ mj/src/app/game/common_define/frame_res.lua
@@ -26,3 +26,49 @@ mjDarkBack = {
 
 mjCardBs = "bs.png"  --桌面上的余牌暗牌 V
 mjCardBh = "bh.png"  --桌面上的余牌暗牌 H
+-- mjAction_G = "action_a_g.png"  --杠
+-- mjAction_H = "action_a_h.png"  --胡
+-- mjAction_P = "action_a_p.png"  --碰
+mjActionBJ = "action_bj.png"  --背景
+mjActionG = {
+	normal = "action_g1.png",
+	pressed = "action_g2.png",
+	disabled = "action_g3.png",
+}
+
+mjActionH = {
+	normal = "action_h1.png",
+	pressed = "action_h2.png",
+	disabled = "action_h3.png",
+}
+
+mjActionP = {
+	normal = "action_p1.png",
+	pressed = "action_p2.png",
+	disabled = "action_p3.png",
+}
+
+--過
+mjActionX = {
+	normal = "action_x1.png",
+	pressed = "action_x2.png",
+	disabled = "action_x3.png",
+}
+
+mjActionWan = {
+	normal = "action_wan1.png",
+	pressed = "action_wan2.png",
+	disabled = "action_wan3.png",
+}
+
+mjActionTong = {
+	normal = "action_tong1.png",
+	pressed = "action_tong2.png",
+	disabled = "action_tong3.png",
+}
+
+mjActionTiao = {
+	normal = "action_tiao1.png",
+	pressed = "action_tiao2.png",
+	disabled = "action_tiao3.png",
+}
