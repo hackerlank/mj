d9c2eb8b9cbfc502b7fa926174d9d4e467d05cb7
WW <746277418@qq.com>
2016-11-30 18:04:15 +0800

    .

--- mj/src/app/game/common_define/GameEnum.lua
+++ mj/src/app/game/common_define/GameEnum.lua
@@ -7,11 +7,51 @@ mjCardType = {
 	mj_hua = 5
 }
 
---牌型定义
-mjPatternType = {
-	unknow = 0,
-	shunzi = 1,
-	kezi = 2,
-	gangzi = 3,
-	jiang = 4
+mjDCardType = {
+	mj_init = 1,
+	mj_dark = 2,  --手持
+	mj_play = 3,  --已出
+	mj_tdark = 4, --未手持（暗杠的暗牌）
+	mj_show = 5,  --未手持（碰、杠牌等）
+}
+
+
+-- 1:本家 2：下家 3：对家 4：上家
+--手牌键值(手上暗牌显示形式)
+mjDarkCardKey = {
+	[1] = "x_",
+	[2] = "by.png",
+	[3] = "bxx.png",
+	[4] = "bz,png"
+}
+
+--打出的牌键值
+mjPlayCardKey = {
+	[1] = "szx_",
+	[2] = "yx_",
+	[3] = "sfx_",
+	[4] = "zx_"
+}
+
+--暗杠暗牌情况(手牌区域)
+mjTDarkCardKey = {
+	[1] = "bs.png",
+	[2] = "bh.png",
+	[3] = "bsx.png",
+	[4] = "bh.png"
+}
+
+--碰、杠、胡等明牌情况（手牌区域）
+mjShowCardKey = {
+	[1] = "sz_",
+	[2] = mjPlayCardKey[2],
+	[3] = mjPlayCardKey[3],
+	[4] = mjPlayCardKey[4]
+}
+
+mjInitCardKey = {
+	[1] = "bsx.png",
+	[2] = "bh.png",
+	[3] = "bsx.png",
+	[4] = "bh.png",
 }
--- mj/src/app/game/ui/modules/Card.lua
+++ mj/src/app/game/ui/modules/Card.lua
@@ -1,11 +1,23 @@
 --所有牌的基類
 local Card = class("Card", function() return display.newSprite() end)
 
+--的手牌
+local kDarkKey = {
+	[1] = "x_%d.png",
+	[2] = 
+}
+
 function Card:ctor()
 	self._id = nil
 	self._name = nil
-	self._type = nil
+	self._type = nil  --条、筒、万
+	self._cardType = nil --牌形式
+	--附加属性
 	self._sortId = 0  --在手牌中的位置
+	self._seat = 0
+	self._isMine = false
+
+	self:setCardType(mjDCardType.mj_init)  --一开始所有的牌都是默认牌
 end		
 
 function Card:changeId(id)
@@ -14,6 +26,86 @@ function Card:changeId(id)
 	self._name = mjCardTxt[self._id]
 	self._type = mjCardType[math.ceil(self._id/9)]
 end
+
+function Card:setCardType(card_type)
+	if card_type < mjDCardType.mj_init or card_type > mjDCardType.mj_show then
+		assert("牌型更改错误 error: Card line: 30")
+	end
+	self._cardType = card_type
+	local card_type_listeners = {
+		[mjDCardType.mj_init] = handler(self, self._changeToInit),
+		[mjDCardType.mj_dark] = handler(self, self._changeToDark),
+		[mjDCardType.mj_play] = handler(self, self._changeToPlay),
+		[mjDCardType.mj_tdark] = handler(self, self._changeToTDark),
+		[mjDCardType.mj_show] = handler(self, self._changeToShow),
+	}
+	card_type_listeners[card_type]()
+end
+
+--初始化牌形式
+function Card:_changeToInit()
+	self:setSpriteFrame(mjInitCardKey[self._seat])
+end
+
+--手牌形式
+function Card:_changeToDark()
+	local card = ""
+	if self._seat == 1 then
+		card = string.format(mjDarkCardKey[1] .. "%d.png", self._id)
+	else
+		card = mjDarkCardKey[self._seat]
+	end
+	self:setSpriteFrame(card)
+end
+
+--出牌形式
+function Card:_changeToPlay()
+	local card = string.format(mjPlayCardKey .. "%d.png", self._id)
+	self:setSpriteFrame(card)
+end
+
+--暗杠暗牌牌情况(手牌区域)
+function Card:_changeToTDark()
+	self:setSpriteFrame(mjTDarkCardKey[self._seat])
+end
+
+--碰、杠、胡明牌情况(手牌区域)
+function Card:_changeToShow()
+	local card = string.format(mjShowCardKey[self._seat] .. "%d.png", self._id)
+	self:setSpriteFrame(card)
+end
+
+function Card:_darkCardTouchListener(event)
+	if event.name == "began" then
+		UIChangeObserver:getInstance():dispatcherUIChangeObserver(ListenerIds.kPlayCard, {card = self, seat = self.m_seat})
+		return false
+	end
+end
+
+--======================================
+
+function Card:setSeat(seat)
+	self._seat = seat
+end
+
+function Card:getSeat()
+	return self._seat
+end
+
+function Card:getSortId()
+	return self._sortId
+end
+
+function Card:setSortId(id)
+	self._sortId = id
+end
+
+function Card:setIsMine(ret)
+	self._isMine = ret
+	self:setTouchEnabled(ret)
+end
+
+--======================================
 
 function Card:getId()
 	return self._id
@@ -25,14 +117,6 @@ end
 
 function Card:getName()
 	return self._name
-end
-
-function Card:getSortId()
-	return self._sortId
-end
-
-function Card:setSortId(id)
-	self._sortId = id
 end
 
 return Card
